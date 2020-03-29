6f8f4bb703c97569211621c8844df8a58dbea807
Nina Sobiczewska <s19048@pjwstk.edu.pl>
2020-03-29 20:15:22 +0200

    commit zad7

--- cw3/Controllers/StudentsController.cs
+++ cw3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cw3.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,34 +14,59 @@ namespace cw3.Controllers
     {
         // GET: api/Students
         [HttpGet]
+
+        public string GetStudent(string orderBy)
+        {
+            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        }
+        /*
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
+        */
 
         // GET: api/Students/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
         {
-            return "value";
+            if (id == 1)
+                return Ok("Kowalski");
+            else if (id == 2)
+                return Ok("Malewski");
+            else
+                return NotFound("Nie znaleziono studenta");
         }
 
         // POST: api/Students
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult CreateStudent(Student student )
         {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
         }
-
         // PUT: api/Students/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult PutStudent(int id)
         {
-        }
+            if (id == 1)
+            {
+                return Ok("Aktualizacja dokończona");
+            }
+            return NotFound("Nie znaleziono studenta o podanym id");
 
+        }
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult RemoveStudent(int id)
         {
+            if (id == 1)
+            {
+                return Ok("Usuwanie ukończone");
+            }
+            return NotFound("Nie znaleziono studenta o danym id");
         }
+        
+       
     }
 }
